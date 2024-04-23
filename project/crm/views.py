8c41569fdce81545d9b6bfba29f9cb34f765ee12
Rubens <rbgabrielgoncalves>
2024-04-23 08:42:32 -0300

    feat: sistema de login completo

--- project/crm/views.py
+++ project/crm/views.py
@@ -1,14 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . forms import CreateUserForm, LoginForm
+
+# - Authentication models and functions
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 def homepage(request):
+
     return render(request, 'crm/index.html')
 
 def register(request):
-    return render(request, 'crm/register.html')
+
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my_login")
+        
+    context = {'registerform':form}
+    return render(request, 'crm/register.html', context=context)
 
 def my_login(request):
-    return render(request, 'crm/my_login.html')
+
+    form = LoginForm()
+    if request.method =='POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+                
+    context = {'loginform':form}
+    return render(request, 'crm/my_login.html', context=context)
 
 def dashboard(request):
+
     return render(request, 'crm/dashboard.html')
+
+def user_logout(request):
+
+    auth.logout(request)
+    return redirect("")
