d59dd63ba3010a148404da36182b071026f85e1a
Rubens <rbgabrielgoncalves>
2024-04-23 08:03:59 -0300

    feat: adicionando o user registration

--- project/crm/urls.py
+++ project/crm/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.homepage, name=""),
+    path('register', views.register, name="register"),
+    path('my_login', views.my_login, name="my_login"),
+    path('dashboard', views.dashboard, name="dashboard"),
 ]
--- project/crm/views.py
+++ project/crm/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homepage(request):
+    return render(request, 'crm/index.html')
+
+def register(request):
+    return render(request, 'crm/register.html')
+
+def my_login(request):
+    return render(request, 'crm/my_login.html')
+
+def dashboard(request):
+    return render(request, 'crm/dashboard.html')
