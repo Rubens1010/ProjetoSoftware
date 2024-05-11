8b6e0cfc6d689d6196814632458468534a4ba786
Rubens <rbgabrielgoncalves>
2024-05-10 22:50:58 -0300

    feat: add bootstrap and improve html code

--- project/crm/forms.py
+++ project/crm/forms.py
@@ -5,23 +5,47 @@ from django import forms
 
 from django.forms.widgets import PasswordInput, TextInput
 
+
 # - Create/Register a user (Model Form)
 class CreateUserForm(UserCreationForm):
+    first_name = forms.CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "First Name"})
+    )
+    last_name = forms.CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "Last Name"})
+    )
+    username = forms.CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "Username"})
+    )
+    email = forms.CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "Email"})
+    )
+    password1 = forms.CharField(
+        widget=PasswordInput(attrs={"class": "form-control", "placeholder": "Password"})
+    )
+    password2 = forms.CharField(
+        widget=PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Confirm password"}
+        )
+    )
 
-    first_name = forms.CharField(widget=TextInput())
-    last_name = forms.CharField(widget=TextInput())
-    username = forms.CharField(widget=TextInput())
-    email = forms.CharField(widget=TextInput())
-    password1 = forms.CharField(widget=PasswordInput())
-    password2 = forms.CharField(widget=PasswordInput())
-    
     class Meta:
-
         model = User
-        fields = ['first_name','last_name','username', 'email', 'password1', 'password2']
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+
 
 # - Authenticate a user (Model Form)
 class LoginForm(AuthenticationForm):
-
-    username = forms.CharField(widget=TextInput())
-    password = forms.CharField(widget=PasswordInput())
+    username = forms.CharField(
+        widget=TextInput(attrs={"class": "form-control", "placeholder": "username"})
+    )
+    password = forms.CharField(
+        widget=PasswordInput(attrs={"class": "form-control", "placeholder": "password"})
+    )
